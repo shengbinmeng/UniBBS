4f96250d23a9d19d434a7e77562902796a88998d
fanyingming <yingmingfan@gmail.com>
2014-10-12 19:54:24 +0800

    controller files is too much, create some groups. Also create mail list view.

---
+++ UniBBS/MailListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MailListTableViewController.h
+//  UniBBS
+//
+//  Created by fanyingming on 10/12/14.
+//  Copyright (c) 2014 Peking University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MailListTableViewController : UITableViewController
+
+@end
