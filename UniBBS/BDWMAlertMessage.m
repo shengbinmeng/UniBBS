24ecfd392f3ff1f67f46a04f0daaa620f2399700
fanyingming <yingmingfan@gmail.com>
2014-10-18 18:41:33 +0800

    Let alert view auto dismiss.

--- UniBBS/BDWMAlertMessage.m
+++ UniBBS/BDWMAlertMessage.m
@@ -14,6 +14,16 @@
 {
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"" message:message delegate:self cancelButtonTitle:@"知道了" otherButtonTitles:nil];
     [alert show];
+    
+    [self performSelector:@selector(dimissAlert:) withObject:alert afterDelay:2.0];
 }
 
++ (void) dimissAlert:(UIAlertView *)alert
+{
+    if(alert)
+    {
+        [alert dismissWithClickedButtonIndex:[alert cancelButtonIndex] animated:YES];
+        [alert release];
+    }
+}
 @end
