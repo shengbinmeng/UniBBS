3128df72a232320a13da47f7f9fdcb63a879a842
fanyingming <yingmingfan@gmail.com>
2016-11-09 22:45:04 +0800

    Refactor BDWMPosting codes

--- UniBBS/BDWMPosting.m
+++ UniBBS/BDWMPosting.m
@@ -12,7 +12,15 @@
 @implementation BDWMPosting
 
 + (void) sendPosting:(NSString *)board WithTitle:(NSString *)title WithContent:(NSString *)content  WithAnonymous:(int)anonymous blockFunction:(void (^)(NSDictionary *responseDict, NSString *error))block {
-    [[BDWMNetwork sharedManager] requestWithMethod:POST WithParams:[NSDictionary dictionaryWithObjectsAndKeys:@"post", @"type", board, @"board", title, @"title", content, @"text", [NSNumber numberWithInt: anonymous], @"anonymous", nil] WithSuccessBlock:^(NSDictionary *dic) {
+    NSDictionary *param = @{
+                            @"type" : @"post",
+                            @"board" : board,
+                            @"title" : title,
+                            @"text" : content,
+                            @"anonymous": [NSNumber numberWithInt: anonymous]
+                            };
+    
+    [[BDWMNetwork sharedManager] requestWithMethod:POST WithParams:param WithSuccessBlock:^(NSDictionary *dic) {
         int code = [[dic objectForKey:@"code"] intValue];
         if (code == 0) {
             block(dic, nil);
@@ -34,7 +42,7 @@
                             @"threadid" : threadid,
                             @"postid" : postid,
                             @"author" : author
-                        };
+                            };
     
     [[BDWMNetwork sharedManager] requestWithMethod:POST WithParams:param WithSuccessBlock:^(NSDictionary *dic) {
         int code = [[dic objectForKey:@"code"] intValue];
